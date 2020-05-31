9ed4cf71d61a41277b22fef8feea664b31a3636a
illfate <illfate254@gmail.com>
2020-05-31 11:41:11 +0300

    Add eulirian path

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -44,6 +44,8 @@ func New(service service.Service) *Server {
 
 	r.HandleFunc("/api/v1/graph/{id:[1-9]+[0-9]*}/hamiltonianPath", s.HamiltonianPath).
 		Queries("startNode", "{startNode}").Methods(http.MethodGet)
+	r.HandleFunc("/api/v1/graph/{id:[1-9]+[0-9]*}/eulerianCycle", s.EulerianCycle).
+		Queries("startNode", "{startNode}").Methods(http.MethodGet)
 
 	r.HandleFunc("/api/v1/graph/{id:[1-9]+[0-9]*}/planarCheck", s.PlanarCheck).Methods(http.MethodGet)
 	r.HandleFunc("/api/v1/graph/list", s.GraphList).Methods(http.MethodGet)
